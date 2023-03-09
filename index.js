53a3f4f5b5190aa0c4ee15a2ba4978580dc173b0
Glitch (greet-bot) <none>
2023-03-09 02:35:15 +0000

    🚤🌨 Checkpoint ./index.js:55570320/1602

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const { App } = require('@slack/bolt');
+const { App,LogLevel } = require('@slack/bolt');
 const store = require('./store');
 
 const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
-  token: process.env.SLACK_BOT_TOKEN
+  token: process.env.SLACK_BOT_TOKEN,
+  logLevel: LogLevel.DEBUG
 });
 
 
@@ -17,59 +18,14 @@ app.event('app_home_opened', async ({ event, say }) => {
       channel: event.channel
     };
     store.addUser(user);
-
-    await say(`Hello world, and welcome <@${user}>!`);
+    await say(`Hello world, and welcome <@${user.user}>!`);
   } else {
     await say('Hi again!');
   }
   
   
-});
+  
 
-app.shortcut('open_modal', async ({ ack, payload, client }) => {
-  // Acknowledge shortcut request
-  ack();
-
-  try {
-    // Call the views.open method using the WebClient passed to listeners
-    const result = await client.views.open({
-      trigger_id: payload.trigger_id,
-      view: {
-        "type": "modal",
-        "title": {
-          "type": "plain_text",
-          "text": "My App"
-        },
-        "close": {
-          "type": "plain_text",
-          "text": "Close"
-        },
-        "blocks": [
-          {
-            "type": "section",
-            "text": {
-              "type": "mrkdwn",
-              "text": "About the simplest modal you could conceive of :smile:\n\nMaybe <https://api.slack.com/reference/block-kit/interactive-components|*make the modal interactive*> or <https://api.slack.com/surfaces/modals/using#modifying|*learn more advanced modal use cases*>."
-            }
-          },
-          {
-            "type": "context",
-            "elements": [
-              {
-                "type": "mrkdwn",
-                "text": "Psssst this modal was designed using <https://api.slack.com/tools/block-kit-builder|*Block Kit Builder*>"
-              }
-            ]
-          }
-        ]
-      }
-    });
-
-    console.log(result);
-  }
-  catch (error) {
-    console.error(error);
-  }
 });
 
 
