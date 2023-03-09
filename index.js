d8a9493846800cba856ab80d7180aa116d31ea0a
Glitch (greet-bot) <none>
2023-03-09 02:24:13 +0000

    ⚓️🐦 Checkpoint ./index.js:55570320/4165

--- index.js
+++ index.js
@@ -22,6 +22,54 @@ app.event('app_home_opened', async ({ event, say }) => {
   } else {
     await say('Hi again!');
   }
+  
+  
+});
+
+app.shortcut('open_modal', async ({ ack, payload, client }) => {
+  // Acknowledge shortcut request
+  ack();
+
+  try {
+    // Call the views.open method using the WebClient passed to listeners
+    const result = await client.views.open({
+      trigger_id: payload.trigger_id,
+      view: {
+        "type": "modal",
+        "title": {
+          "type": "plain_text",
+          "text": "My App"
+        },
+        "close": {
+          "type": "plain_text",
+          "text": "Close"
+        },
+        "blocks": [
+          {
+            "type": "section",
+            "text": {
+              "type": "mrkdwn",
+              "text": "About the simplest modal you could conceive of :smile:\n\nMaybe <https://api.slack.com/reference/block-kit/interactive-components|*make the modal interactive*> or <https://api.slack.com/surfaces/modals/using#modifying|*learn more advanced modal use cases*>."
+            }
+          },
+          {
+            "type": "context",
+            "elements": [
+              {
+                "type": "mrkdwn",
+                "text": "Psssst this modal was designed using <https://api.slack.com/tools/block-kit-builder|*Block Kit Builder*>"
+              }
+            ]
+          }
+        ]
+      }
+    });
+
+    console.log(result);
+  }
+  catch (error) {
+    console.error(error);
+  }
 });
 
 
