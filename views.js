1d9bf1bb29f33c600a3495b86808649f1f4f8fa7
Glitch (greet-bot) <none>
2023-03-09 15:47:51 +0000

    😀🎳 Checkpoint ./views.js:55570320/2671

--- views.js
+++ views.js
@@ -1,62 +1,61 @@
-exports.modalView = () => {
+exports.modalView = () =>  {
   return {
-    title: {
-      type: "plain_text",
-      text: "#ええやんカウンター",
-    },
-    type: "modal",
-    blocks: [
-      {
-        type: "section",
-        text: {
-          type: "plain_text",
-          text: "期間を選択してください。",
-          emoji: true,
-        },
-      },
-      {
-        block_id: "date-start",
-        type: "input",
-        element: {
-          type: "datepicker",
-          initial_date: "2023-1-1",
-          placeholder: {
-            type: "plain_text",
-            text: "Select a date",
-            emoji: true,
-          },
-          action_id: "datepicker-action",
-        },
-        label: {
-          type: "plain_text",
-          text: "いつから",
-          emoji: true,
-        },
-      },
-      {
-        block_id: "date-end",
-        type: "input",
-        element: {
-          type: "datepicker",
-          initial_date: "2023-1-31",
-          placeholder: {
-            type: "plain_text",
-            text: "Select a date",
-            emoji: true,
-          },
-          action_id: "datepicker-action",
-        },
-        label: {
-          type: "plain_text",
-          text: "いつまで",
-          emoji: true,
-        },
-      },
-    ],
-    submit: {
-      type: "plain_text",
-      text: "計測",
-      action_id: "count_start",
-    },
-  };
-};
+	"title": {
+		"type": "plain_text",
+		"text": "#ええやんカウンター"
+	},
+	"submit": {
+		"type": "plain_text",
+		"text": "計測",
+	},
+	"blocks": [
+		{
+			"type": "section",
+			"text": {
+				"type": "plain_text",
+				"text": "期間を選択してください。",
+				"emoji": true
+			}
+		},
+		{
+      "block_id": "date-start",
+			"type": "input",
+			"element": {
+				"type": "datepicker",
+				"initial_date": "2023-1-1",
+				"placeholder": {
+					"type": "plain_text",
+					"text": "Select a date",
+					"emoji": true
+				},
+				"action_id": "datepicker-action"
+			},
+			"label": {
+				"type": "plain_text",
+				"text": "いつから",
+				"emoji": true
+			}
+		},
+		{
+      "block_id": "date-end",
+			"type": "input",
+			"element": {
+				"type": "datepicker",
+				"initial_date": "2023-1-31",
+				"placeholder": {
+					"type": "plain_text",
+					"text": "Select a date",
+					"emoji": true
+				},
+				"action_id": "datepicker-action"
+			},
+			"label": {
+				"type": "plain_text",
+				"text": "いつまで",
+				"emoji": true
+			}
+		}
+	],
+	"type": "modal"
+}
+}
